85c5aec5cab70f8c9b59d668c136d48b7484d090
thanhnam1986 <namxt86gpt@gmail.com>
2026-08-30 21:49:00 +0700

    Xóa bộ nhớ đệm cũ trên điện thoại

--- sw.js
+++ sw.js
@@ -1,47 +1,12 @@
-// SERVICE WORKER - My Apps (Thành Nam)
-// Tăng phiên bản mỗi lần cập nhật để điện thoại bỏ cache cũ.
-const CACHE_VERSION = 'v4';
-const CACHE_NAME = `myapps-cache-${CACHE_VERSION}`;
-
-const APP_SHELL = [
-  './',
-  './index.html',
-  './personal/index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
-  self.skipWaiting();
-});
+// Bản v5: gỡ bộ nhớ đệm PWA cũ để My Apps luôn lấy bản mới từ GitHub.
+self.addEventListener('install', () => self.skipWaiting());
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    (async () => {
-      const keys = await caches.keys();
-      await Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)));
-      await self.clients.claim();
-      const windows = await self.clients.matchAll({ type: 'window' });
-      await Promise.all(windows.map((client) => client.navigate(client.url)));
-    })()
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  if (new URL(event.request.url).origin !== self.location.origin) return;
-
-  event.respondWith(
-    fetch(new Request(event.request, { cache: 'no-store' }))
-      .then((response) => {
-        if (response.ok) {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        }
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((key) => caches.delete(key)));
+    const windows = await self.clients.matchAll({ type: 'window' });
+    await self.registration.unregister();
+    await Promise.all(windows.map((client) => client.navigate(client.url)));
+  })());
 });
